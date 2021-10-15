e59c5084bb7eb1681126b1dc2747cdb096395ae1
Yathavan Prabhakar <68110471+yadprab@users.noreply.github.com>
2021-10-15 13:24:37 +0530

    Update useOuterHTML.tsx

--- src/Hooks/useOuterHTML.tsx
+++ src/Hooks/useOuterHTML.tsx
@@ -1,7 +1,7 @@
 import { RefObject, useEffect, useState } from "react";
 const useOuterHTML = <T extends RefObject<HTMLElement>>(ref: T) => {
   const [HTML, setHTML] = useState<string | undefined>("");
-  console.log(ref);
+  
 
   useEffect(() => {
     const res = ref.current?.outerHTML;
